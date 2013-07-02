323e0ebea27cdf2830da8d25e5e79ecc24e1e8b7
Dylan O'Carroll <dylancocarroll@gmail.com>
2013-07-02 10:29:07 -0700

    cleanup

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -2,6 +2,5 @@ var helper = require('./lib/html-fetcher-helpers');
 // eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
 setInterval(function(){
-  console.log('fetching sites...');
   helper.readUrls(__dirname +'/../data/sites.txt',helper.downloadUrl);
 }, 120000);
